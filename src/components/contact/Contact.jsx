5aa7a58740ee37dce5d64c7f47f018d52c51090c
Sandeep Kumar <sandeepkumar@Sandeeps-MacBook-Air-2.local>
2022-07-11 22:47:06 +0530

    firstcommit1

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import "./Contact.css"
-
+import { useRef as UseRef} from 'react';
+import emailjs from "emailjs-com"
 
 const contact = () => {
+  const form = UseRef();
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_thr8gxj', 'template_p6jwe86', form.current,"9F8Nq9f3jnBHlp4L7")
+      e.target.reset()
+    
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+
   return (
     <section id='contact'>
       <h5>Get in Touch (I am lonely!)</h5>
       <h2>Contact Me</h2>
       <div className='container contact_container'>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name='message' rows="7" placeholder="Your Message" required></textarea>
