3f55eab9ae44767f7b4cef3f3927bcd7bac17b8e
Sandeep Kumar <65333883+imsandeepk@users.noreply.github.com>
2022-10-31 15:13:58 +0530

    final

--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Cta = () => {
   return (
     <div className='Cta'>
-        <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' className='btn'>Download CV</a>
+        <a href='https://drive.google.com/file/d/1lRxM5CchppqNXPtGtJchSL-g7-GgBPaa/view?usp=share_link' className='btn'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
