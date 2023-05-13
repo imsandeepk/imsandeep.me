36ed1c9df24aef0246ae0d90f9ad1f7b37e30ccf
Sandeep Kumar <65333883+imsandeepk@users.noreply.github.com>
2023-05-14 01:53:23 +0530

    Update Cta.jsx

--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {GiCoffeeCup} from "react-icons/gi"
+import {GrDocument} from "react-icons/gr"
 
 const Cta = () => {
   return (
     <div className='Cta'>
-        <a href='https://drive.google.com/file/d/1lRxM5CchppqNXPtGtJchSL-g7-GgBPaa/view?usp=share_link' className='btn'>Download CV</a>
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        <a href='https://drive.google.com/file/d/1lRxM5CchppqNXPtGtJchSL-g7-GgBPaa/view?usp=share_link' className='btn'>Download CV <GrDocument size={15}/></a>
+        <a href='https://bmc.link/iamsandeep' className='btn btn-primary'>Buy Me A Coffee <GiCoffeeCup size={25}/></a>
+        
     </div>
   )
 }
